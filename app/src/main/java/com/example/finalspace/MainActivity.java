2496ec687f70921e746e045974c2a2c79f1c1e88
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-04 02:25:35 +0300

    add changes

--- app/src/main/java/com/example/finalspace/MainActivity.java
+++ app/src/main/java/com/example/finalspace/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindSeasonButton;
@@ -18,13 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFindSeasonButton = (Button)findViewById(R.id.findseasonsButton);
+        @BindView(R.id.findseasonsButton) Button mFindSeasonsButton;
+//        mFindSeasonButton = (Button)findViewById(R.id.findseasonsButton);
 //        mSeasonEditText = (EditText) findViewById(R.id.seasons);
 
         mFindSeasonButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
                 Toast.makeText(MainActivity.this, "Watch Out!", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, FindActivity.class);
 //                Log.d(TAG, season)
