1646e2e3d8d48de3f36f34e4d61cf9ed1bbb2300
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-03 23:22:17 +0300

    updated FindActivity

--- app/src/main/java/com/example/finalspace/FindActivity.java
+++ app/src/main/java/com/example/finalspace/FindActivity.java
@@ -2,14 +2,21 @@ package com.example.finalspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class FindActivity extends AppCompatActivity {
-
+    private String[] seasons = new String[]{
+            "season one", "Season two", "Season three"
+    };
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
+
+        mListView =(ListView) findViewById(R.id.list);
     }
 }
--- app/src/main/java/com/example/finalspace/MainActivity.java
+++ app/src/main/java/com/example/finalspace/MainActivity.java
@@ -4,25 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindSeasonButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mFindSeasonButton = (Button)findViewById(R.id.findseasonsButton);
+//        mSeasonEditText = (EditText) findViewById(R.id.seasons);
 
         mFindSeasonButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Watch Out!", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(MainActivity.this, FindActivity.class);
+//                Log.d(TAG, season)
                 startActivity(intent);
             }
         });
