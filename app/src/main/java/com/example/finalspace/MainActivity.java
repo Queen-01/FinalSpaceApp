c8b2dc3ecb438a8352f39a5ef29d0d057679b80a
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-04 22:10:45 +0300

    made changes

--- app/src/main/java/com/example/finalspace/MainActivity.java
+++ app/src/main/java/com/example/finalspace/MainActivity.java
@@ -1,32 +1,33 @@
 package com.example.finalspace;
 
-import androidx.appcompat.app.AppCompatActivity;
+import  androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindSeasonButton;
+//    public static final String TAG = MainActivity.class.getSimpleName();
+//    private Button mFindSeasonButton;
+
+    @BindView(R.id.findseasonsButton) Button mFindSeasonButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        @BindView(R.id.findseasonsButton) Button mFindSeasonsButton;
 //        mFindSeasonButton = (Button)findViewById(R.id.findseasonsButton);
 //        mSeasonEditText = (EditText) findViewById(R.id.seasons);
 
         mFindSeasonButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
 
@@ -37,4 +38,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
